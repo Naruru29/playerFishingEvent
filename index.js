9b9dc31ea708348953e3ae5c07d3be5f40739efb
Naruru29 <132210942+Naruru29@users.noreply.github.com>
2024-02-16 10:21:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-import { world, system } from "@minecraft/server";
-import playerFishingAfterEvent from "./lib/FishingEvent";
 
-playerFishingAfterEvent.subscribe(ev => {
-    const { player, itemStack, dimension, result } = ev;
-
-    /*
-    player: プレイヤーがを返します
-    itemStack: アイテムが釣れた時のみ、itemStackを返します
-    dimension: 釣りをしたプレイヤーのディメンションを検知
-    result: 釣りに成功したかどうか
-    */
-});
